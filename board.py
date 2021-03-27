c717e6ecb5daf9cf1210869f6c9581640562bdbc
Ben Life <77246839+benlife5@users.noreply.github.com>
2021-03-27 14:08:45 -0400

    Removed interpreter

--- board.py
+++ board.py
@@ -4,7 +4,7 @@ import pygame as pg
 def main():
     pg.init()
     running = True
-    pg.display.set_caption("Sorry!")
+    pg.display.set_caption("HooHacks")
     screen_width, screen_height = pg.display.Info().current_w, pg.display.Info().current_h
     min_dimension = min(screen_width, screen_height)
     surf = pg.display.set_mode((int(min_dimension / 1.5), int(min_dimension / 1.5)), pg.RESIZABLE, pg.SCALED)
